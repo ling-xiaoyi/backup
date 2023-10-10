94547898c67555f01e65fadd24353ee76a20d8eb
ling-xiaoyi <2080204533@qq.com>
2023-10-10 14:58:28 +0800

    更新 script.js

--- 2023.10.9/Visual Studio Code/@javascript/01/js/script.js
+++ 2023.10.9/Visual Studio Code/@javascript/01/js/script.js
@@ -1,3 +1,27 @@
+var i
+var num
+for (i=1; i<=100; i++) {
+   sum =sum + i
+}
+console.log("i");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // var a;
 // var b;
 // a = 1;
@@ -8,18 +32,18 @@
 //   alert(b);
 // }
 // f();
-var bianliang;
+// var bianliang;
 
 
 
 
-var x = "123";
-var y = 123;
+// var x = "123";
+// var y = 123;
 
-x === y;
-console.log(x == y);
+// x === y;
+// console.log(x == y);
 
-console.log(x == y);
+// console.log(x == y);
 
 
-document.getElementById("box_1").innerHTML=bianliang;
+// document.getElementById("box_1").innerHTML=bianliang;
